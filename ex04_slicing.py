91629775793f2beb3e4aa8a71a47f0ddfaa8e2c4
Ajay Kumar S <143310521+AjayKumarOnly@users.noreply.github.com>
2024-12-25 14:49:21 +0530

    Add files via upload
    slicing of string

---
+++ ex04_slicing.py
@@ -0,0 +1,6 @@
+userInput = input("Enter the String : ")
+
+print(f"Original String : {userInput}")
+print(f"Type 1 : {userInput[0:5]}")
+print(f"Type 2 : {userInput[0:8:2]}")
+print(f"Type 3 : {userInput[:-3]}")
