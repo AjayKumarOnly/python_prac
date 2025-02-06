a462b5a360562cd75ec18d15f9bf10b2de5af1d6
Ajay Kumar S <143310521+AjayKumarOnly@users.noreply.github.com>
2025-02-06 19:21:43 +0530

    Update ex05.py
    The input file is add for the whole  operation

--- ex05.py
+++ ex05.py
@@ -1,15 +1,21 @@
-userInput = input("Enter the String : ")
+file = open("read.txt","r")
+
+all = file.read()
 
 vowelCount = 0
 spaceCount = 0
 charCount = 0
-for i in userInput:
+lineCount = 0
+for i in all:
     if i.lower() in "aeiou":
         vowelCount += 1
-    elif i.isspace():
+    if i.isspace():
         spaceCount += 1
+    if i == '\n':
+        lineCount += 1
     charCount+=1
 
 print(f"vowelCount : {vowelCount}")
 print(f"spaceCount : {spaceCount}")
 print(f"charCount : {charCount}")
+print(f"Line count : {lineCount}")
